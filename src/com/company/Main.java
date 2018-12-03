4a739c5b60cdf63beba5c4e36b5d0ac85d5b7e67
seemal <schauhan1206@gmail.com>
2018-12-02 19:00:48 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int a = 62;
+	int b = 18;
+	System.out.println("The sum is " + Integer.sum(a,b));
+
+
+    }
+}
